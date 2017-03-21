d21fc6dad9beadea9e6d0144eae58244fde42012
Brunaldo Toshkezi <brunaldo.toshkezi@shish.gov.al>
2017-03-21 12:06:12 +0100

    Blog seo

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -18,8 +18,8 @@ class BlogController extends Controller
 
     }
 
-    public function  show($id){
-        $post=Post::findOrFail($id);
+    public function  show(Post $post){
+      // $post=Post::published()->findOrFail($id);
         return view('blog.show',compact('post'));
     }
 }
