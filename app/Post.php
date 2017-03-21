7e2923ff4b3e7c5aafecc5156ba7e964cb703b48
Brunaldo Toshkezi <brunaldo.toshkezi@shish.gov.al>
2017-03-21 13:18:41 +0100

    Markdown support

--- app/Post.php
+++ app/Post.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use GrahamCampbell\Markdown\Facades\Markdown;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
@@ -23,9 +24,17 @@ class Post extends Model
        return is_null($this->published_at) ? '' : $this->published_at->diffForHumans();
     }
 
+    public function  getBodyHtmlAttribute($value){
+        return $this->body ? Markdown::convertToHtml(e($this->body)): NULL ;
+    }
+    public function  getExcerptHtmlAttribute($value){
+        return $this->excerp ? Markdown::convertToHtml(e($this->excerp)): NULL ;
+    }
+
     public function scopeLatestFirst($query){
         return $query->orderBy('created_at','desc');
     }
+
 
     public function scopePublished($query){
         return $query->where("published_at","<=", Carbon::now());
